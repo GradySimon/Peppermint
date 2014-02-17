06031199fd205db7ed6c1032ab5610c61f0e2c7c
Grady Simon <grady@gradysimon.com>
2014-02-17 18:28:24 -0500

    Started adding more sophistocated data loading logic (queuing topics).

--- peppermint/src/main/java/com/gradysimon/peppermint/UpstreamDataManager.java
+++ peppermint/src/main/java/com/gradysimon/peppermint/UpstreamDataManager.java
@@ -3,13 +3,24 @@ package com.gradysimon.peppermint;
 import android.util.Log;
 
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * Created by grady on 2/14/14.
  */
 public class UpstreamDataManager {
 
-    public static List<Topic> getTopicList() {
+    private static UpstreamDataManager instance;
+
+    public static UpstreamDataManager getInstance() {
+        if (instance == null) {
+            instance = new UpstreamDataManager();
+        }
+        return instance;
+    }
+
+    public static List<Topic> getTopicListFromUpstream() {
         Log.i("Server Interaction", "Initiating GET against the topic list API using JSON.");
         List<Topic> topicList = JsonApiManager.getTopicList();
         Log.i("Server Interaction", "GET against the topic list API using JSON complete");
@@ -20,5 +31,31 @@ public class UpstreamDataManager {
             Log.w("Server Interaction", "Got no results from JSON API on getTopicList().");
         }
         return topicList;
+    }
+
+    private Queue<Topic> topicQueue;
+
+    private Queue<Topic> topicQueue() {
+        if (topicQueue == null) {
+            topicQueue = new PriorityQueue<Topic>();
+            refreshTopicQueue();
+        }
+        return topicQueue;
+    }
+
+    private void refreshTopicQueue() {
+        topicQueue().clear();
+        topicQueue().addAll(getTopicListFromUpstream())
+    }
+
+    public Topic getNextTopic() {
+        if (topicQueue().isEmpty()) {
+            refreshTopicQueue();
+        }
+        return topicQueue().poll();
+    }
+
+    private UpstreamDataManager() {
+
     }
 }
